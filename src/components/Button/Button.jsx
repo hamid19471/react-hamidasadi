694e57f8dcb8af4e986c6e53d041cea91cb58b4d
hamid19471 <hamid.19471@gmail.com>
2023-05-04 19:53:02 +0330

    Update: remove interface

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,16 @@
-import { useNavigate } from "react-router-dom";
-
-
-const Button = ({ name, path, primary }) => {
-    const navigate = useNavigate();
-    const handleCliclButton = () => {
-        navigate(`${path}`);
-    };
-    return (
-        <button
-            className={` text-content font-extralight py-2 px-5 rounded-lg duration-300 uppercase tracking-wider ${primary
-                ? "bg-primary text-black hover:scale-110"
-                : "border border-primary text-white hover:bg-primary hover:scale-110 hover:text-black"
-                } `}
-            onClick={handleCliclButton}
-        >
-            {name}
-        </button>
-    );
+const Button = ({ name, onClick, primary }) => {
+  return (
+    <button
+      className={` text-content font-extralight py-2 px-5 rounded-lg duration-300 uppercase tracking-wider ${
+        primary
+          ? "bg-primary text-black hover:scale-110"
+          : "border border-primary text-white hover:bg-primary hover:scale-110 hover:text-black"
+      } `}
+      onClick={onClick}
+    >
+      {name}
+    </button>
+  );
 };
 
 export default Button;
