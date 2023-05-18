914a778ad20755199bb0cffccf1d41d374e9da52
hamid19471 <hamid.19471@gmail.com>
2023-05-18 20:46:18 +0330

    Update: menu path for portfolio

--- src/FakeAPI/MenuItem.js
+++ src/FakeAPI/MenuItem.js
@@ -1,22 +1,22 @@
 export const menuItems = [
-    {
-        id: 1,
-        name: "Home",
-        link: "/",
-    },
-    {
-        id: 2,
-        name: "About",
-        link: "/about-me",
-    },
-    {
-        id: 3,
-        name: "Portfolio",
-        link: "/portfolio",
-    },
-    {
-        id: 4,
-        name: "Contact",
-        link: "/contact-me",
-    },
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    link: "/about-me",
+  },
+  {
+    id: 3,
+    name: "Portfolio",
+    link: "/portfolios",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    link: "/contact-me",
+  },
 ];
