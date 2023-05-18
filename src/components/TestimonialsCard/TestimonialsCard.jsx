b58d2602f90ca57af6ff49917db514d52784ba56
hamid19471 <hamid.19471@gmail.com>
2023-05-18 20:49:23 +0330

    Complete: testimonials and feching from bakc-end

--- src/components/TestimonialsCard/TestimonialsCard.jsx
+++ src/components/TestimonialsCard/TestimonialsCard.jsx
@@ -1,26 +1,25 @@
 const TestimonialsCard = ({ item }) => {
-    return (
-        <div className="flex flex-col p-[30px] items-center justify-center bg-secondray border m-3 rounded-lg min-h-[450px]">
-            <img
-                src={
-                    import.meta.env.VITE_URL +
-                    item.attributes.image.data.attributes.url
-                }
-                alt={item.attributes.name}
-                width={"100px"}
-                className="rounded-full border-8 border-white"
-            />
-            <h1 className="mt-[20px] font-light text-2xl tracking-wider my-[5px]">
-                {item.attributes.name}
-            </h1>
-            <h2 className="font-extralight text-[13px] tracking-widest uppercase">
-                {item.attributes.position}
-            </h2>
-            <p className="font-extralight tracking-wide leading-8 text-white/80 mt-[20px] text-center">
-                {item.attributes.comment}
-            </p>
-        </div>
-    );
+  return (
+    <div className="flex flex-col p-[30px] items-center justify-center bg-secondray border m-3 rounded-lg min-h-[450px]">
+      <img
+        src={
+          import.meta.env.VITE_URL + item.attributes.image.data.attributes.url
+        }
+        alt={item.attributes.name}
+        width={"100px"}
+        className="rounded-full border-8 border-white"
+      />
+      <h1 className="mt-[20px] font-light text-2xl tracking-wider my-[5px]">
+        {item.attributes.name}
+      </h1>
+      <h2 className="font-extralight text-[13px] tracking-widest uppercase">
+        {item.attributes.position}
+      </h2>
+      <p className="font-extralight tracking-wide leading-8 text-white/80 mt-[20px] text-center">
+        {item.attributes.comment}
+      </p>
+    </div>
+  );
 };
 
 export default TestimonialsCard;
