e8311ba332b3239150934c61189dcc7014fa830e
hamid19471 <hamid.19471@gmail.com>
2023-05-18 20:45:27 +0330

    Update: add to top page code and use in browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Contact from "./pages/Contact/Contact";
+import SinglePortfolio from "./pages/SinglePortfolio/SinglePortfolio";
+import { useEffect } from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
 function App() {
-    return (
-        <div className="h-screen">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="about-me" element={<About />} />
-                    <Route path="portfolio" element={<Portfolio />} />
-                    <Route path="contact-me" element={<Contact />} />
-                    <Route path="*" element={<div>404 Error</div>} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="h-screen">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about-me" element={<About />} />
+          <Route path="portfolios" element={<Portfolio />} />
+          <Route path="works/:id" element={<SinglePortfolio />} />
+          <Route path="contact-me" element={<Contact />} />
+          <Route path="*" element={<div>404 Error</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
