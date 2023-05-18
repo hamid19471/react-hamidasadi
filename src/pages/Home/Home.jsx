94212c5d575bd10e41c3d6a1e5fe0a645bec4809
hamid19471 <hamid.19471@gmail.com>
2023-05-18 20:48:15 +0330

    Update: add portfolio items by choosing number of card as props

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,22 +3,24 @@ import Clients from "../../components/Clients/Clients";
 import ContactMe from "../../components/ContactMe/ContactMe";
 import Experiences from "../../components/Expreriences/Experiences";
 import HearoSection from "../../components/HeroSection/HearoSection";
+import Portfolios from "../../components/Portfolios/Portfolios";
 import Services from "../../components/Services/Services";
 import Testimonial from "../../components/Testimonial/Testimonial";
 import Layout from "../../layout";
 
 const Home = () => {
-    return (
-        <Layout>
-            <HearoSection />
-            <AboutMe />
-            <Clients />
-            <Experiences />
-            <Services />
-            <Testimonial />
-            <ContactMe />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <HearoSection />
+      <AboutMe />
+      <Clients />
+      <Experiences />
+      <Services />
+      <Portfolios slice={3} />
+      <Testimonial />
+      <ContactMe />
+    </Layout>
+  );
 };
 
 export default Home;
