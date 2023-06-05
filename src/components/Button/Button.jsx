88ded5e0b0679e42b7686c5cfd64cb323b07dd78
hamid19471 <hamid.19471@gmail.com>
2023-06-05 11:23:25 +0330

    Update: add class for dynamic hidden button in other components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
-const Button = ({ name, onClick, primary }) => {
+const Button = ({ name, onClick, primary, hidden }) => {
   return (
     <button
       className={` text-content font-extralight py-2 px-5 rounded-lg duration-300 uppercase tracking-wider ${
         primary
           ? "bg-primary text-black hover:scale-110"
           : "border border-primary text-white hover:bg-primary hover:scale-110 hover:text-black"
-      } `}
+      } ${hidden ? "hidden" : ""} `}
       onClick={onClick}
     >
       {name}
