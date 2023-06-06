889cd2adca2ca336497aa7fb95b3f8778094163f
hamid19471 <hamid.19471@gmail.com>
2023-06-06 18:10:31 +0330

    Create: basic loading animation until fetch compleye

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import "./Loading.style.css";
+const Loading = () => {
+  return (
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Loading;
